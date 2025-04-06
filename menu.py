388b20d5eccd0fdc7573fffebe05ab7382514293
Jorge Hernández <106907504+JorgeHI@users.noreply.github.com>
2025-04-06 17:22:51 +0200

    First beta version 0.1.0b1
    Script open and save
    Script highlighting
    Seach by nodes and knobs and invalid characters.
    Wrap text

---
+++ menu.py
@@ -0,0 +1,8 @@
+import nkScriptEditor
+
+nukescripts.registerWidgetAsPanel(
+    "nkScriptEditor.add_nk_script_editor_panel",
+    "Nk Script Editor",
+    "jorgehi.nkScriptEditor",
+    True
+)
---
+++ nkScriptEditor.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------
+# Nk Script Editor for Nuke
+# Copyright (c) 2025 Jorge Hernandez Ibañez
+#
+# This file is part of the Nk Script Editor project.
+# Repository: https://github.com/tu-usuario/NkScriptEditor
+#
+# This software is licensed under the MIT License.
+# See the LICENSE file in the root of this repository for details.
+# -----------------------------------------------------------------------------
+
+import nukescripts
+import nksePanel
+
+def add_nk_script_editor_panel():
+    return nksePanel.NkScriptEditor()
+
+nukescripts.registerWidgetAsPanel(
+    "nkScriptEditor.add_nk_script_editor_panel",
+    "Nk Script Editor",
+    "jorgehi.nkScriptEditor",
+    True
+)
